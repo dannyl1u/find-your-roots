3935ede84b18c21428ff9c7d541f11418fa6e595
dla216 <danny_liu_5@sfu.ca>
2023-03-04 11:41:44 -0800

    connect react to flask

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,16 @@
 from flask import (Flask, render_template)
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 app = Flask(__name__)
 
 
 @app.route('/index')
+@cross_origin()
 def get_tree():
-    return "hello world"
+    return {"name": "bob"}
 
 
 if __name__ == '__main__':
