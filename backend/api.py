576aab1b39eaea9549b4aa9ccf7816ccb5c40059
katie <katiesun138@gmail.com>
2023-03-04 12:19:01 -0800

    button added on default html page

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__, template_folder="templates")
@@ -14,9 +14,11 @@ def get_person(personId):
     return ("the person you returned is " + personId)
 
 
-@app.route('/addNode')
+@app.route('/addNode', methods=['POST', 'GET'])
 def add_person():
-    return "adding a new person"
+    output = request.form.to_dict()
+    name = output["name"]
+    return render_template('temp.html', name=name)
 
 
 # @app.route()
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
 
 function App() {
