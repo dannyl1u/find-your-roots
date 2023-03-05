34a3cff846fbf6f37c6fb704c28d471d29cb1465
dla216 <danny_liu_5@sfu.ca>
2023-03-04 18:24:28 -0800

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import { useState, useEffect } from 'react';
 // import {Person} from 'components/Person'
 import FamilyTree from './mytree'
 
+
+let personId = 1;
+let prevPersonId = 0;
+
 function App() {
   const [data, setData] = useState([])
   useEffect(() => {
@@ -14,7 +18,10 @@ function App() {
       res.json().then((data) => {
           // Setting a data from api
           setData(data)
-          console.log(data)
+
+          console.log(data[0])
+          console.log(data[1])
+
       })
 
       
@@ -23,24 +30,59 @@ function App() {
 , []);
 
   return (
-    <FamilyTree nodes={ data &&
-      data.map((data) => (
-        { id: 1, pids: [2], name: 'asdf', gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
-        { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
-        { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
-        { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
-        { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
-      ))
-  } />
+  //   <h1>hello</h1>
+  // )
+      // <div>{data && data.map}</div>
   //   <div>
-  //   {data && (
-  //     <ul>
-  //       {data.map(person => (
-  //         <li> {person.name} {person.age}</li>
-  //       ))}
-  //     </ul>
-  //   )}
+  //   <FamilyTree nodes={
+  //     data.map((e) => (
+
+  //        { id: 1, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  }
+        
+  //       //{ id: 1, pids: [2], name: "asdf", gender: 'female'  }
+  //       // { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+  //       // { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+  //       // { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+  //       // { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' ]
+  //     ))
+  // } /> 
   // </div>
+  
+//   data && <FamilyTree nodes={ [
+
+//       { id: 1, pids: [2], name: data.name, gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
+//       { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+//       { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+//       { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+//       { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
+//   ]
+// } />
+
+//   <FamilyTree nodes={ [
+//       { id: 1, pids: [2], name: data.name, gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
+//       { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+//       { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+//       { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+//       { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
+//   ]
+// } />
+
+    <div>
+    {data && (
+      <ul>
+        {data.map(person => {
+          // personId++
+
+          //<li> {person.name} {person.age} {person.id} </li>
+          return <FamilyTree nodes={
+            [{id: person.id, pids: [person.id], name: person.name, gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'}]}/>
+            
+          })}
+        
+        
+      </ul>
+    )}
+  </div>
 
   );
 }
