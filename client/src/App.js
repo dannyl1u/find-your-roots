7037c25f8a2699c3ad7580ca19c1c61e4c2d9c91
dla216 <danny_liu_5@sfu.ca>
2023-03-04 16:02:40 -0800

    adding

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,46 @@ import logo from './logo.svg';
 import React, { Component }  from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
+// import {Person} from 'components/Person'
+import FamilyTree from './mytree'
 
 function App() {
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
   useEffect(() => {
   // Using fetch to fetch the api from 
   // flask server it will be redirected to proxy
   fetch("http://localhost:5000/getNodes").then((res) =>
       res.json().then((data) => {
           // Setting a data from api
-          setData(data[0])
-          console.log(data.name)
+          setData(data)
+          console.log(data)
       })
+
+      
   );
-}, []);
+}
+, []);
 
   return (
-    <div className="App">
-      <h1>
-        Name: {data && data.name}
-      </h1>
-      <h2>
-        Age: {data && data.age}
-      </h2>
+    <FamilyTree nodes={ data &&
+      data.map((data) => (
+        { id: 1, pids: [2], name: 'asdf', gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
+        { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+        { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+        { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+        { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
+      ))
+  } />
+  //   <div>
+  //   {data && (
+  //     <ul>
+  //       {data.map(person => (
+  //         <li> {person.name} {person.age}</li>
+  //       ))}
+  //     </ul>
+  //   )}
+  // </div>
 
-    </div>
   );
 }
 
